5f956c5ae0bffa9ca8cd496d5cbb51aaf62817ac
Surendra994919 <162982493+Surendra994919@users.noreply.github.com>
2024-03-11 16:35:32 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,107 @@
+day one
+
+#print("hello world")
+#print("hello 1")
+
+print("hello world")
+
+"""
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+"""
+
+-----> commminting
+1.commenting is used for making documentation in apython file
+2.when the line is commented, that will not be considered as a part of the
+  code
+3.when the progarm is executed commented line will not be in the part of
+  excution.
+
+--->note;
+
+import this
+# import is a keyboard which is used to communicated b/w the python files.
+
+import antigravity
+
+11/3 1:00 to 5:00
+Bargained characters
+
+ Controls and Data structures--> ABC language
+ Strings and its formats --> from C
+ Regular expressions --> pearl
+ Exception handling --> Module 3
+ Magic methods from --> fortron
+
+WHAT kind of language is python
+
+ Python is a
+
+ *Object oriented programming language
+ *Dynamically typed language
+ *Scripting language
+ *Interpreted language
+ *Compiled language
+ *Function programming language
+
+ Flavours of python
+
+ ->Cpython - Written in C, Most common implementation of python.
+ ->Jython - Java implementation of python.
+ ->Ironpyhon - implementation of the Python programming language which is tightly integrated with NET.
+ ->Brython - Browser python runs in browser.
+ ->Rubypython - Bridge b/w python and Ruby interpreters
+ ->Micropython - runs in micro controllers.
+
+ #print(object,sep,end,file,flush)this are the parameters
+ #print has 5 parameters
+
+ 
+ Object---> the vaiue to be printed is object
+ sep--> seperator
+ #Eg
+ # a = 8
+ # b = 9
+ # c = 89
+ # print(a,b,c,  sep='$')
+
+ # end --> which is used to add the string at the end of the value,
+ # # It will not keep the command to stay in same line
+ # Eg:
+ # print(str(a),b, end='%%')
+ # print(str(c))
+
+ #  ! ---> variables
+ # ?variable is a name which is used to identify the value of variable
+ # ?which is stored in a specific memory location
+
+ 
+ RULES TO DEDFINE VARIABLES
+1.variable ahve to start with only alphabets underscores
+2.the char following the 1st char can be alphabets , numbers,underscores
+3.no spaces are allowed
+4.no special characters like !@#$%^&*()+
+5.case sensitive. 6.keywords are not allowed as variables
+
+# !ways to defne variables
+
+# a = 8
+# b = 9
+#print(a,b)
+
+# a=5,6, 9.46, "hello"
+# print(a)
+# print(type(a))
+
+ 
+ 
+
+
+
+
+
